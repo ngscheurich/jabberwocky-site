557add4dd9fd3fcf4c37ca75179ccec2505ab308
Nicholas Scheurich <nick@scheurich.me>
2018-02-23 18:42:26 -0600

    Some changes?

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,29 @@ import Link from "gatsby-link";
 import tachyons from "tachyons";
 import jabberwocky from "../static/images/jabberwocky.jpg";
 
+console.log(jabberwocky);
+
 const IndexPage = () => (
-  <div className="flex">
+  <div className="flex tc">
     <div
       className="dn db-l contain"
-      style={{ backgroundImage: jabberwocky, height: "100vh" }}
+      style={{
+        backgroundImage: `url(${jabberwocky})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "50vw"
+      }}
     />
 
-    <div>
-      <h1>Text BLIGHT to (225) 123-4567</h1>
-      <p>or the Jabberwocky will devour you!</p>
+    <div
+      className="pa4 flex items-center"
+      style={{ height: "100vh", width: "50vw" }}
+    >
+      <div className="">
+        <h1 style={{ fontSize: "5rem" }}>Text BLIGHT to (225) 240-4652</h1>
+        <p style={{ fontSize: "3rem" }}>or the Jabberwocky will devour you!</p>
+      </div>
     </div>
   </div>
 );
